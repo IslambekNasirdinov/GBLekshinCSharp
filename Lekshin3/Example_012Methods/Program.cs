2ea6df47de86ac9c250494e1d4b407f04bc798f9
Islambek Nasirdinov <islambeknasirdinov@gmail.com>
2023-05-16 20:48:55 +0300

    Лексия 3 пример 1

---
+++ Lekshin3/Example_012Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string text = "Материал из Википедии — свободной энциклопедии"
+            +"У этого термина существуют и другие значения, см. Текст (значения)."
+            +"Пример текста"
+            +"Текст (от лат. textus — ткань; сплетение, сочетание) — зафиксированная на каком-либо материальном носителе "
+            +"Существуют две основные трактовки понятия «текст»: имманентная (расширенная, философски нагруженная) и" 
+            +"В лингвистике термин «текст» используется в широком значении, включая и образцы устной речи. Восприятие "; 
+string Replace(string text, char oldValue, char newValue){
+    string result = String.Empty;
+    
+    int length = text.Length;
+    for (int i = 0; i<length; i++){
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
